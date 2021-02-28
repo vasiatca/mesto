edb471617fda13f4f4de59dcf653ffff5328f96c
Alex <vasiatca@yandex.ru>
2021-02-28 18:04:15 +0300

    проектная работа 4.3

--- scripts/index.js
+++ scripts/index.js
@@ -4,41 +4,49 @@ let popupContainer = document.querySelector('.popup__container');
 let closePopupButton = document.querySelector('.popup__close');
 let body = document.querySelector('.body');
 
-let likeButtons = document.querySelectorAll('.element__like');
+let form = document.querySelector(".popup__content");
+let nameInput = document.querySelector('[name="name-input"]');
+let jobInput = document.querySelector('[name="job-input"]');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubTitle = document.querySelector('.profile__subtitle');
 
-editPopupButton.addEventListener('click', toggleShowPopup);
-closePopupButton.addEventListener('click', toggleShowPopup);
-popup.addEventListener('click', toggleShowPopup);
-popupContainer.addEventListener('click', stopPropagation);
+// let likeButtons = document.querySelectorAll('.element__like');
 
 function toggleShowPopup() {
+  if (nameInput.value === '') {
+    nameInput.value = profileTitle.textContent;
+  }
+
+  if (jobInput.value === '') {
+    jobInput.value = profileSubTitle.textContent;
+  }
+
   popup.classList.toggle('popup_opened');
   body.classList.toggle('body_disabled-scroll'); 
 }
 
-function stopPropagation(event) {
-  event.stopPropagation();
-}
-
-for (let i = 0; i < likeButtons.length; i++) {
-  likeButtons[i].addEventListener('click', toggleActiveLikeButton);
-}
+// for (let i = 0; i < likeButtons.length; i++) {
+//   likeButtons[i].addEventListener('click', toggleActiveLikeButton);
+// }
 
 function toggleActiveLikeButton(event) {
   event.target.classList.toggle('element__like_active');
 }
 
-let form = document.querySelector(".popup__content");
-let nameInput = document.querySelector('[name="nameInput"]');
-let jobInput = document.querySelector('[name="jobInput"]');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubTitle = document.querySelector('.profile__subtitle');
+function submit(event) {
+  event.preventDefault();
+  
+  profileTitle.textContent = nameInput.value;
+  profileSubTitle.textContent = jobInput.value;
+  
+  toggleShowPopup();
+}
 
 form.addEventListener('submit', submit);
 
-function submit(event) {
-  event.preventDefault();
-  profileTitle.textContent = nameInput.value;
-  profileSubTitle.textContent = jobInput.value;
-  toggleShowPopup();
-}
+editPopupButton.addEventListener('click', toggleShowPopup);
+closePopupButton.addEventListener('click', toggleShowPopup);
+popup.addEventListener('click', toggleShowPopup);
+popupContainer.addEventListener('click', function(event) {
+  event.stopPropagation();
+});
